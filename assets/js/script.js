b246964c2d9856210c225c68c09e102523db4590
Rosa Turner <rmturner1220@gmail.com>
2022-05-16 21:41:52 -0400

    submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var taskToDoE1 = document.querySelector("#task-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+    
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task.";
     taskToDoE1.appendChild(listItemE1);
 };
 
-buttonE1.addEventListener("click", createTaskHandler);
+
+
+formE1.addEventListener("submit", createTaskHandler);
+
+
 
