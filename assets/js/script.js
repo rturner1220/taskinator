3e7f2d5c5395ce5662cbd3ef41ffbeb952ced218
Rosa Turner <rmturner1220@gmail.com>
2022-05-17 00:27:01 -0400

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,48 @@
 var formE1 = document.querySelector("#task-form");
 var taskToDoE1 = document.querySelector("#task-to-do");
 
-var createTaskHandler = function(event) {
-
+var taskFormHandler = function(event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // check if input are empty (validate)
+    if (taskNameInput === "" || taskTypeInput === "") {
+        alert("You need to fill out the task form!");
+        return false;
+    }
     
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
-    taskToDoE1.appendChild(listItemE1);
-};
+formE1.reset();
 
+   // reset form fields for next task to be entered
+  document.querySelector("input[name='task-name']").value = "";
+  document.querySelector("select[name='task-type']").selectedIndex = 0;
 
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
 
-formE1.addEventListener("submit", createTaskHandler);
+    createTaskEl(taskDataObj); 
+    };
+
+    var createTaskEl = function(taskDataObj) {
+     //create list item
+     var listItemE1 = document.createElement("li");
+     listItemE1.className = "task-item";
+
+   // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  listItemE1.appendChild(taskInfoEl);
+  console.dir(listItemE1);
+
+         // add list item to list
+  taskToDoE1.appendChild(listItemE1);
+    };
+
+ formE1.addEventListener("submit", taskFormHandler);
 
 
 
